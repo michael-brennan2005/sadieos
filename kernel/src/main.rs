cc4d8d0fa0b087d2f5cf194cd0fc5758f405cb1b
Michael Brennan <michael.brennancolorado@gmail.com>
2026-03-13 15:53:35 -0400

    For fuck sake start over

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,80 +1,8 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
-use core::panic::PanicInfo;
-// use sd_kernel::{allocator, memory::{self, BootInfoFrameAllocator}, println, process::Process, serial_println};
-use bootloader_api::{BootInfo, BootloaderConfig, config::Mapping, entry_point};
-use x86_64::{VirtAddr, structures::paging::{Page, PageTable, Translate}};
-
-// extern crate alloc;
-// use alloc::boxed::Box;
-
-// pub mod allocator;
-// pub mod gdt;
-// pub mod interrupts;
-//pub mod memory;
-// pub mod process;
-// pub mod serial;
-// pub mod vga_buffer;
-
-pub static BOOTLOADER_CONFIG: BootloaderConfig = {
-    let mut config = BootloaderConfig::new_default();
-    config.mappings.physical_memory = Some(Mapping::Dynamic);
-    config
-};
-
-entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
-
-#[unsafe(no_mangle)]
-fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    // Init framebuffer
-    // if let Some(fb) = boot_info.framebuffer.take();
-
-
-    // serial_println!("Hello World{}", "!");
-    
-    // Init code
-    // gdt::init();
-    // interrupts::init_idt();
-    // unsafe { interrupts::PICS.lock().initialize(); };
-    // x86_64::instructions::interrupts::enable();
-
-    
-    // let program = include_bytes!("../../userland/hello-asm/hello");
-    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    // println!("PTR -> {:#X}", (&phys_mem_offset as *const VirtAddr) as u64);
-    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    // let mut frame_allocator = unsafe {
-    //     BootInfoFrameAllocator::init(&boot_info.memory_map)
-    // };
-
-    // println!("--- Memory map ---");
-    // for region in boot_info.memory_map.iter() {
-    //     serial_println!("region.range = {:?}, region.region_type = {:?}", region.range, region.region_type);
-    // }
-
-    // new
-    // allocator::init_heap(&mut mapper, &mut frame_allocator)
-    //     .expect("heap initialization failed");
-
-    // Process::init_from_elf_bytes(&mut frame_allocator, phys_mem_offset, program);
-
-    // println!("It did not crash!");
-    hlt_loop();
-}
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    // println!("{}", info);
-    hlt_loop();
-}
-
-fn hlt_loop() -> ! {
-    loop {
-        x86_64::instructions::hlt();
-    }
-}
+use bootloader_api::{BootInfo, entry_point};
+use core::fmt::Write;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
@@ -83,11 +11,38 @@ pub enum QemuExitCode {
     Failed = 0x11,
 }
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
+pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+    use x86_64::instructions::{nop, port::Port};
 
     unsafe {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+
+    loop {
+        nop();
+    }
+}
+
+pub fn serial() -> uart_16550::SerialPort {
+    let mut port = unsafe { uart_16550::SerialPort::new(0x3F8) };
+    port.init();
+    port
+}
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    let mut port = serial();
+    writeln!(port, "Entered kernel with boot info: {boot_info:?}").unwrap();
+    writeln!(port, "\n=(^.^)= meow\n").unwrap();
+    exit_qemu(QemuExitCode::Success);
+}
+
+/// This function is called on panic.
+#[panic_handler]
+#[cfg(not(test))]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    let _ = writeln!(serial(), "PANIC: {info}");
+    exit_qemu(QemuExitCode::Failed);
 }
